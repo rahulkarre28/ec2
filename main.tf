aa5c24942caba4f8115cef452d0d0cde4769db6f
rahulkarre28 <karrerahul8@gmail.com>
2023-05-19 13:52:52 -0400

    new files added

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  availability_zone = var.availability_zone
+  hibernation = var.hibernation
+  get_password_data = var.get_password_data
+  disable_api_stop = var.disable_api_stop
+
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "instance_type" {
+    type = string
+}
+
+variable "availability_zone" {
+    type = string
+}
+
+variable "hibernation" {
+    type = bool
+}
+
+variable "disable_api_stop" {
+    type = bool
+}
+
+variable "get_password_data" {
+    type = bool
+}
+variable "ami" {
+    type = string
+}
